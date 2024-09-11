548048f7f77dfb773730195f6563e2e55b9757e7
Jonathan Göransson <jonathan.a.goransson@outlook.com>
2024-09-11 15:35:56 +0200

    added functionality to connect to mongodb server

---
+++ db/database.mjs
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+const databaseName = "wpp";
+const collectionName = "docs";
+
+const database = {
+  getDb: async function getDb() {
+    let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@jsramverk.nd6p6.mongodb.net/${databaseName}?retryWrites=true&w=majority&appName=jsramverk`;
+
+    if (process.env.NODE_ENV === "test") {
+      dsn = "mongodb://localhost:27017/test";
+    }
+
+    const client = await MongoClient.connect(dsn, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = await client.db();
+    const collection = await db.collection(collectionName);
+
+    return {
+      collection: collection,
+      client: client,
+    };
+  },
+};
+
+export default database;
--- routes/index.mjs
+++ routes/index.mjs
@@ -1,20 +1,91 @@
 import express from "express";
+import database from "../db/database.mjs";
+import { ObjectId } from "mongodb";
 var router = express.Router();
 
-router.get("/user", (req, res) => {
-  res.json({
-    data: {
-      msg: "Got a GET request, sending back default 200",
-    },
-  });
+let db;
+
+// const doc = {
+//     name: body.name,
+//     html: body.html,
+// };
+
+// const result = await db.collection.insertOne(doc);
+
+// if (result.result.ok) {
+//     return res.status(201).json({ data: result.ops });
+// }
+
+// const ObjectId = require('mongodb').ObjectId;
+
+// const filter = { _id: ObjectId(body["_id"]) };
+// const updateDocument = {
+//     name: body.name,
+//     html: body.html,
+// };
+
+// const result = await db.collection.updateOne(
+//     filter,
+//     updateDocument,
+// );
+
+router.get("/user", async (req, res) => {
+  try {
+    const { collection } = await database.getDb();
+    const keyObject = await collection.findOne({ name: "John Doe" });
+
+    console.log(keyObject);
+
+    if (keyObject) {
+      res.status(200).json({ data: keyObject });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (e) {
+    res.status(500).json({
+      errors: {
+        status: 500,
+        source: "/",
+        title: "Database error",
+        detail: e.message,
+      },
+    });
+  } finally {
+    if (db && db.client) {
+      await db.client.close();
+    }
+  }
 });
 
-router.post("/user", (req, res) => {
-  res.status(201).json({
-    data: {
-      msg: "Got a POST request, sending back 201 Created",
-    },
-  });
+router.post("/user", async (req, res) => {
+  try {
+    const { collection } = await database.getDb();
+
+    collection.insertOne({
+      name: "John Doe",
+      age: 30,
+      email: "johndoe@example.com",
+    });
+
+    res.status(201).json({
+      data: {
+        msg: "Got a POST request, sending back 201 Created",
+      },
+    });
+  } catch (e) {
+    res.status(500).json({
+      errors: {
+        status: 500,
+        source: "/",
+        title: "Database error",
+        detail: e.message,
+      },
+    });
+  } finally {
+    if (db && db.client) {
+      await db.client.close();
+    }
+  }
 });
 
 router.put("/user", (req, res) => {
